ad54551d4f201444b671c27540247f0603ef199f
Der Critter <daboubet@gmail.com>
2018-02-15 23:53:35 +0100

    Add files via upload

---
+++ Polyrain 1.0/Scripts/CollisionScipt/CollisionScript9.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionScript9 : MonoBehaviour {
+
+
+	public AudioSource collisionSound;
+	public GameObject EnemyParticle;
+
+	// Use this for initialization
+	void Start () {
+		collisionSound = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	private void OnCollisionEnter (Collision collision)
+	{
+
+		if(collision.gameObject.tag == "target"){
+			
+
+	    FindObjectOfType<AudioManager> ().Play ("Note9");
+
+		Instantiate(EnemyParticle,transform.position,Quaternion.identity);
+
+		//ScaleCubes();
+
+		//sound.Play();
+		//??transform.position = Vector3.one * 9999f;
+		Destroy(gameObject);
+
+	}
+
+	//private void OnCollisionStay(Collision collision){
+		
+
+}
+}
---
+++ Polyrain 1.0/Scripts/superRandomSpawner2.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class superRandomSpawner2 : MonoBehaviour {
+
+	public GameObject[] spawnees;
+	public GameObject[] spawnPoints;
+
+	/// The height all objects need to be spawned at.
+	public float spawnPositionY;
+
+	public float radius = 2;
+	public float spawnTime = 600f;
+
+	int spawneeIndex;
+	int spawnPointIndex;
+	Vector3 spawnPosition;
+
+	void Start() {
+		InvokeRepeating ("Spawn", 2, 2);
+
+		spawnPoints = GameObject.FindGameObjectsWithTag("spawnPoint");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		SpawnRandom();
+	}
+
+	int GetRandom(int count) {
+		return Random.Range(0, count);
+	}
+
+	Vector3 GetRandomVector (Vector3 vec) {
+		Vector3 randomVector = (Random.insideUnitSphere* radius) + vec;
+		return new Vector3(randomVector.x, spawnPositionY, randomVector.z);
+	}
+
+	void SpawnRandom() {
+		spawneeIndex = GetRandom(spawnees.Length);
+		spawnPointIndex = GetRandom(spawnPoints.Length);
+		spawnPosition = GetRandomVector(spawnPoints[spawnPointIndex].transform.position);
+		Instantiate(spawnees[spawneeIndex], spawnPosition, spawnPoints[spawnPointIndex].transform.rotation);
+	}
+}
